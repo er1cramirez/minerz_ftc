6140fb39790513b0c82b0fd95fd8f7638697e4e0
Eric <ericdramirez@tec.mx>
2025-10-26 22:50:20 -0600

    Add motors setup to constructor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -1,27 +1,48 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.drivebase.MecanumDrive;
+import com.seattlesolvers.solverslib.geometry.Pose2d;
+import com.seattlesolvers.solverslib.hardware.motors.Motor;
+import com.seattlesolvers.solverslib.hardware.motors.Motor.ZeroPowerBehavior;
+import com.seattlesolvers.solverslib.hardware.motors.MotorEx;
 
 public class DriveSubsystem extends SubsystemBase {
+    private final MotorEx frontRightMotor;
+    private final MotorEx backLeftMotor;
+    private final MotorEx backRightMotor;   
+
     private final MecanumDrive drive;
 
+
     public DriveSubsystem() {
-        drive = new MecanumDrive(
-            new Motor(hardwareMap, "frontLeft", Motor.GoBILDA.RPM_435),
-            new Motor(hardwareMap, "frontRight", Motor.GoBILDA.RPM_435),
-            new Motor(hardwareMap, "backLeft", Motor.GoBILDA.RPM_435),
-            new Motor(hardwareMap, "backRight", Motor.GoBILDA.RPM_435)
-        );
+        // Individual motors for configuration
+        MotorEx frontLeftMotor = new MotorEx(hardwareMap, "frontLeft", Motor.GoBILDA.RPM_435);
+        frontLeftMotor.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+        frontRightMotor = new MotorEx(hardwareMap, "frontRight", Motor.GoBILDA.RPM_435);
+        frontRightMotor.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+        backLeftMotor = new MotorEx(hardwareMap, "backLeft", Motor.GoBILDA.RPM_435);
+        backLeftMotor.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+        backRightMotor = new MotorEx(hardwareMap, "backRight", Motor.GoBILDA.RPM_435);
+        backRightMotor.setZeroPowerBehavior(ZeroPowerBehavior.BRAKE);
+        drive = new MecanumDrive(frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor);
+        configureDrivetrain();
     }
 
 
     public void drive(double forward, double strafe, double rotate, boolean fieldRelative) {
         if (fieldRelative) {
-            drive.driveFieldRelative(forward, strafe, rotate, getHeading());
+            drive.driveFieldCentric(forward, strafe, rotate, getHeading(), true);
         } else {
-            drive.driveRobotRelative(forward, strafe, rotate);
+            drive.driveRobotCentric(forward, strafe, rotate, true);
         }
         
+    }
+
+    void configureDrivetrain() {
+
     }
 
     public void stop() {
