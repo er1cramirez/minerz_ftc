ab34610b648aed4121915ff645988de9468c0598
Eric <ericdramirez@tec.mx>
2025-12-06 19:32:59 -0600

    Ejector Subsystem v1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/EjectorConstants.java
@@ -0,0 +1,13 @@
+// XConstants.java
+package org.firstinspires.ftc.teamcode.constants;
+
+public class EjectorConstants {
+    
+    // Hardware names
+    public static final String SERVO_NAME = "ejectorServo";
+
+    // Posiciones del servo del eyector en valores normalizados (0.0 a 1.0)
+    public static final double STOW_POSITION = 0.0;
+    public static final double EJECT_POSITION = 0.5;
+    
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/EjectorSubsystem.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.hardware.servos.ServoEx;
+
+import org.firstinspires.ftc.teamcode.constants.EjectorConstants;
+
+public class EjectorSubsystem extends SubsystemBase {
+
+    public enum EjectorState {
+        STOWED,
+        EJECTING,
+        RETURNING 
+    }
+
+    private EjectorState currentState;
+
+    // Hardware
+    private ServoEx ejectorServo;
+
+    // Constructor
+    public EjectorSubsystem() {
+        currentState = EjectorState.STOWED;
+        ejectorServo = new ServoEx(hardwareMap, EjectorConstants.SERVO_NAME);
+        ejectorServo.set(EjectorConstants.STOW_POSITION);
+    }
+
+    public void eject() {
+        currentState = EjectorState.EJECTING;
+        ejectorServo.set(EjectorConstants.EJECT_POSITION);
+    }
+    public void stow() {
+        currentState = EjectorState.RETURNING;
+        ejectorServo.set(EjectorConstants.STOW_POSITION);
+        currentState = EjectorState.STOWED;
+    }
+
+    public EjectorState getState() { return currentState; }
+    public boolean isStowed() { return currentState == EjectorState.STOWED; }
+}
